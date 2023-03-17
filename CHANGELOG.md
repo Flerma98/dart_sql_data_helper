0cd5a9eaceb6ab37af7ed1adccd4f06a7184f43a
Fernando Lerma <fiernandoalberto@gmail.com>
2023-03-17 15:35:07 -0700

    v1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.1.1
+### Updated
+- WHERE ignore if is null
+
 ## 1.1.0
 ### Updated
 - DATE and DATETIME data type was added
--- lib/sql_where.dart
+++ lib/sql_where.dart
@@ -8,14 +8,14 @@ class SqlWhere {
   SqlWhere(this.clauses,
       {required this.clausesValues, this.whereUnion = WhereUnion.and});
 
-  String getClauses({final bool withText = false}) {
+  String? getClauses({final bool withText = false}) {
+    if (clauses.isEmpty) return null;
+
     for (var i = 0; i < clauses.length; i++) {
       if (clauses[i].contains("()")) {
         clauses[i] = clauses[i].replaceAll("()", "(NULL)");
       }
     }
-
-    if (clauses.isEmpty) return "";
 
     return [if (withText) " WHERE ", clauses.join(" ${whereUnion.name} ")]
         .join(" ");
